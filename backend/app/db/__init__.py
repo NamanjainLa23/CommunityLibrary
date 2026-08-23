2af58624d0489688e40e8ea7901216a474eef7ec
NamanjainLa23 <namanjain363@gmail.com>
2026-08-23 18:45:19 +0530

    backend and frontend free deployement

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -14,7 +14,9 @@ PASSWORD = os.getenv("password")
 HOST = os.getenv("host")
 PORT = os.getenv("port")
 DBNAME = os.getenv("dbname")
+
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
 engine = create_engine(DATABASE_URL)
 
 
