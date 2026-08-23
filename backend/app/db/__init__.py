94749d6e2ef80fd270fa009cf1682f7772e1e451
NamanjainLa23 <namanjain363@gmail.com>
2026-08-23 18:30:44 +0530

    backend and frontend free deployement

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -4,10 +4,19 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./booklender.db")
 
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
+# DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./booklender.db")
+# connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+# engine = create_engine(DATABASE_URL, connect_args=connect_args)
+
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
